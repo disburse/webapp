3ed46fa995cbac061e8aec07ddbfc1da4f7f7b47
disburse <john.matsikas@gmail.com>
2020-10-16 09:06:27 -0600

    testing web3 connection

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const Web3 = require("web3");
+const ethEnabled = () => {
+
+  if (window.ethereum) {
+    window.web3 = new Web3(window.ethereum);
+    window.ethereum.enable();
+    return true;
+  }
+  else if (window.web3) {
+    window.web3 = new Web3(window.web3.currentProvider);
+    window.ethereum.enable();
+    return true;
+  }
+  return false;
+}
+
+if (!ethEnabled()) {
+  alert("Please install an Ethereum-compatible browser or extension like MetaMask to use this dApp!");
+}
+else{
+  alert("Compatible browser found.");
+}
+
 class App extends Component {
 
   render() {
