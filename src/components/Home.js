4873923183bf63e71857f6754ee900253f481fe0
John Matsikas <john.matsikas@gmail.com>
2020-12-11 14:20:23 -0700

    netlify debug attemp #2

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,39 @@
 import React from 'react';
-import Web3 from "web3";
-import Web3Connect from "web3connect";
-import WalletConnectProvider from "@walletconnect/web3-provider";
-//import Web3Modal from "web3modal";
 
 const Home = () => {
+
+    /*
+    import Web3 from "web3";
+    import Web3Connect from "web3connect";
+    import WalletConnectProvider from "@walletconnect/web3-provider";
+
+    // Replaces web3connect
+    // import Web3Modal from "web3modal";
+
+    <Web3Connect.Button
+        network="mainnet"  // optional
+        providerOptions={{
+            walletconnect: {
+                package: WalletConnectProvider, // required
+                options: {
+                    infuraId: "753227e9030d4656a9ef958f23d61c44" // required
+                }
+            }
+        }}
+        onConnect={(provider) => {
+            const web3 = new Web3(provider); // add provider to web3
+        }}
+        onClose={() => {
+            console.log("Web3Connect Modal Closed"); // modal has closed
+        }}
+    />
+    */
 
     return (
        <div>
           <h1>Home</h1>
            <p>Test Web3 Connection</p>
-            <Web3Connect.Button
-                network="mainnet"  // optional
-                providerOptions={{
-                    walletconnect: {
-                        package: WalletConnectProvider, // required
-                        options: {
-                            infuraId: "753227e9030d4656a9ef958f23d61c44" // required
-                        }
-                    }
-                }}
-                onConnect={(provider) => {
-                    // Temporarily commenting out to correct netlify problem:
-                    // Line 24:27:  'web3' is assigned a value but never used  no-unused-vars
-                    // const web3 = new Web3(provider); // add provider to web3
-                }}
-                onClose={() => {
-                    console.log("Web3Connect Modal Closed"); // modal has closed
-                }}
-            />
+           
        </div>
     );
 }
